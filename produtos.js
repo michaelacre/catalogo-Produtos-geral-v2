a2024551aee4e48116b8b567837e83c72e3dd2a5
michaelacre <1sr.soviterios1@gmail.com>
2026-04-23 13:31:24 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -38,7 +38,7 @@ const PRODUTOS = [
     imgs: [
         "https://i.postimg.cc/g2TVSyBd/106.png",
         "https://link-da-segunda-imagem.png",
-        "https://i.postimg.cc/FzkPFTfk/20615-SABBOR-Presunto-3-6-kg-mockup.png"],"
+        "https://i.postimg.cc/FzkPFTfk/20615-SABBOR-Presunto-3-6-kg-mockup.png"],
     desc: "Frango inteiro resfriado, já temperado e pronto para assar, sem miúdos, garantindo praticidade, sabor e suculência no preparo."
 },
   {
